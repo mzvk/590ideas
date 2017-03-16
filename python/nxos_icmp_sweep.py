1a4a39c5aa68b582d60c095a417cf73763c840ec
mzvk <m.zukey@gmail.com>
2017-03-16 20:22:46 +0100

    cosmetic changes

--- python/nxos_icmp_sweep.py
+++ python/nxos_icmp_sweep.py
@@ -2,7 +2,7 @@
 
 import re, threading
 from sys import argv, exit
-from xml.dom import minidom 
+from xml.dom import minidom
 
 ver = '0.5.2b [23:36 11.03.2017]'
 
@@ -19,7 +19,7 @@ def needhelp(argv):
     print '<interface>'
     print 'all'
     exit()
-	
+
 class PingThrd(object):
   status = {'alive': [], 'dead': 0}
   thread_cout = 4
@@ -27,7 +27,7 @@ class PingThrd(object):
   prefix = None
   masklen = None
   generator = None
-  
+
   def ping(self, ipv4Host):
     result = re.search(r'([0-9.]+%) packet loss', cli('ping '+ipv4Host+' vrf '+ifDict[ifname]['ifvrf']+' timeout 1 count 2'))
     return 1 if not result.group(1) == '100.00%' else 0
@@ -56,17 +56,17 @@ class PingThrd(object):
     (self.prefix, self.masklen) = (argprefix, argmasklen)
     subnet = self.prefix & ((2 ** 32 - 1) - (2 ** (32 - self.masklen) - 1))
     print '+ ICMP sweep for: {}/{} - {}'.format(int2str(subnet), self.masklen, ifDict[ifname]['ifnme'])
-    self.generator = hostgen(subnet, self.masklen) 
+    self.generator = hostgen(subnet, self.masklen)
     for i in range(self.thread_count):
       thrd = threading.Thread(target=self.dequeue)
       thrd.start()
       jobs.append(thrd)
     [ thrd.join() for thrd in jobs ]
     return self.status
-    
+
 def redalert(txt):
   return '\x1b[31;01m'+txt+'\x1b[00m'
-  
+
 def str2int(ipStr):
   ipStr = ipStr.split('.')
   return 0 | (int(ipStr[0]) << 24) | (int(ipStr[1]) << 16) | (int(ipStr[2]) << 8) | int(ipStr[3])
@@ -75,22 +75,22 @@ def int2str(ipInt):
   return str(ipInt >> 24 & 0xff)+'.'+str(ipInt >> 16 & 0xff)+'.'+str(ipInt >> 8 & 0xff)+'.'+str(ipInt & 0xFF)
 
 def parsearg(argv):
-  iflist = []  
+  iflist = []
   for arg in argv:
     for rgxp in ifRgxPtn:
       rgx = re.match(rgxp, arg.lower())
-      if rgx: 
+      if rgx:
         iflist.append(rgx.group(1) + rgx.group(2))
         break
     if not rgx: print redalert('[Error]')+' Invalid interface name: {} - ignoring.'.format(arg)
-  if len(iflist) < 1: 
+  if len(iflist) < 1:
     print redalert('[Error]')+' No valid interfaces supplied. -- (terminated)'
-    exit()  
+    exit()
   return iflist
-	
+
 def hostgen(subnet, masklen):
   shift = 1 if masklen < 30 else 0
-  ipv4 = subnet + 1 if masklen < 30 else subnet  
+  ipv4 = subnet + 1 if masklen < 30 else subnet
   while ipv4 <= subnet | (2 ** (32 - masklen) - 1 - shift):
     yield ipv4
     ipv4 += 1
@@ -106,12 +106,12 @@ def node2txt(node):
     nodetext=node[0].firstChild.data.strip()
     return nodetext
   except IndexError:
-    return "__na__"  
+    return "__na__"
 
 def popifDict():
   xml = cli2xml('show ip interface vrf all')
   ifDict = {}
-  
+
   for ifs in zip(xml.getElementsByTagName('ROW_intf'), xml.getElementsByTagName('ROW_vrf')):
     if not node2txt(ifs[0].getElementsByTagName('intf-name')) == '__na__':
       xml = cli2xml('show ip interface '+node2txt(ifs[0].getElementsByTagName('intf-name')))
@@ -128,7 +128,7 @@ def popifDict():
          iflnk=node2txt(ifs[0].getElementsByTagName('link-state')))
       ifDict[ifname]['ifmln'] = ifDict[ifname]['ifmln'][0]
     else:
-      print redalert('[Error]')+' Hit unknown index while parsing xml node.'	  
+      print redalert('[Error]')+' Hit unknown index while parsing xml node.'
   return ifDict
 
 ##############################
@@ -138,7 +138,7 @@ if len(argv) - 1 < 1:
   print 'Version: {} {} (A544778)'.format(ver, redalert('MZvk'))
   exit()
 
-if 'all' in argv: 
+if 'all' in argv:
   print '+ Option \'ALL\' selected, sweeping through all interfaces.'
   iflist = ['all']
 else:
@@ -147,23 +147,23 @@ else:
 ifDict = popifDict()
 
 print '+ Interfaces configured for IPv4: '
-print '-----------------------------------------------------------------'
+print '-' * 65
 print '| ifname         | prefix          | status         | vrf'
 print '|                |                 | (amd/prt/lnk)  |'
-print '-----------------------------------------------------------------'
+print '-' * 65
 for ifn in ifDict:
    if not 'all' in iflist: pri = '*' if ifn in iflist else ' '
    else: 
      pri = '*'
      iflist.append(ifn)
    print redalert(pri)+'{:<15}  {:<16}  {:<16} {}'.format(ifDict[ifn]['ifnme'], ifDict[ifn]['ifpfx'],
-                                                          ifDict[ifn]['ifadm'] +'/'+ ifDict[ifn]['ifprt'] 
+                                                          ifDict[ifn]['ifadm'] +'/'+ ifDict[ifn]['ifprt']
                                                           +'/'+ ifDict[ifn]['iflnk'], ifDict[ifn]['ifvrf'])
    if len(ifDict[ifn]['ifsec']) > 0: 
      for sec in ifDict[ifn]['ifsec']:
-       print redalert(pri)+'{:<15}  {:<16}  {:<16} {}'.format(' \_secondary_ ', 
+       print redalert(pri)+'{:<15}  {:<16}  {:<16} {}'.format(' \_secondary_ ',
                                                               sec, ' --- ', ' --- ')
-print '-----------------------------------------------------------------'
+print '-' * 65
 if iflist[0] == 'all': iflist.pop(0)
 
 ping = PingThrd()
