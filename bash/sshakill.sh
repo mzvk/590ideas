11848dfd4cea823a5e1469fc1e5fcea1e87f898b
mzvk <m.zukey@gmail.com>
2017-08-29 20:18:29 +0200

    kickuser added

--- bash/sshakill.sh
+++ bash/sshakill.sh
@@ -14,18 +14,22 @@ fi
 
 echo -e "\n- checking process list [ssh-agent]"
 pids=($(ps -aux | grep -E "ssh-agent -s$" | awk '{ print $2 }'))
+
 echo "- number of elements in array: ${#pids[@]}"
 echo "- closing ssh-agent processes"
 for i in "${pids[@]}"; do
   $sudo kill -9 $i
 done
+
 sigkills=${#pids[@]}
 echo -e "\n- checking process list [apache2]"
 pids=($(ps -aux | grep -E "apache2 -k start$" | awk '{ print $2 }'))
+
 echo "- number of elements in array ${#pids[@]}"
 echo "- closing apache2 processes"
 for i in "${pids[@]}"; do
   $sudo kill -9 $i
 done
+
 sigkills=$(($sigkills + ${#pids[@]}))
 echo -e "\n- closed $sigkills processes"; echo
