eaf01cd7a6801234944dce20de0b17280af9642c
Maciej <m.zukey@gmail.com>
2018-02-19 11:04:31 +0100

    Update list_login_shells.sh

--- bash/list_login_shells.sh
+++ bash/list_login_shells.sh
@@ -6,11 +6,13 @@
 
 if [[ $# -lt 1 ]]; then
   declare -A pidmap
+  pidc=0
   for x in `ps aux | grep -e "\-bash$" | awk '{print $1":"$2}'`; do
     x=(${x//:/ })
     if [[ ${pidmap[${x[0]}]+_} ]]; then pidmap[${x[0]}]+=" ${x[1]}"; else pidmap[${x[0]}]+=${x[1]}; fi
-  done
-  echo "Found ${#pids[@]} active login shell(s) for ${#pidmap[@]} user(s)"
+    ((pidc++))
+done
+  echo "Found $pidc active login shell(s) for ${#pidmap[@]} user(s)"
   for usr in ${!pidmap[@]}; do
     printf " %-10s: \e[31m%s\e[0m\n" "$usr" "${pidmap[$usr]}"
   done && echo
