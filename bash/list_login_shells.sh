8bc58ab5537f597d6c8e0495d2be76144e010aaf
Maciej <m.zukey@gmail.com>
2018-03-07 14:18:06 +0100

    Update list_login_shells.sh

--- bash/list_login_shells.sh
+++ bash/list_login_shells.sh
@@ -7,7 +7,7 @@
 if [[ $# -lt 1 ]]; then
   declare -A pidmap
   pidc=0
-  for x in `ps aux | grep -e "\-bash$" | awk '{print $1":"$2}'`; do
+  for x in `ps aux | grep -E -- "\-(ba)?sh$" | awk '{print $1":"$2}'`; do
     x=(${x//:/ })
     if [[ ${pidmap[${x[0]}]+_} ]]; then pidmap[${x[0]}]+=" ${x[1]}"; else pidmap[${x[0]}]+=${x[1]}; fi
     ((pidc++))
@@ -20,7 +20,7 @@ else
   uarg=($(echo "$@" | tr ' ' '\n' | sort -u | tr '\n' ' '))
   for arg in "${uarg[@]}"; do
     if [[ -z $(getent passwd | grep -e "^$arg:") ]]; then echo -e "\e[33mUser $arg does not exist on this system.\e[0m" && continue; fi
-    psu=`ps -fu $arg | grep "\-bash$" | awk '{print $2}'`
+    psu=`ps -fu $arg | grep -E -- "\-(ba)?sh$" | awk '{print $2}'`
     if [[ -z $psu ]]; then echo -e "\e[33mUser $arg does not have active login shells.\e[0m" && continue; fi
     echo -e "$arg - ${#pids[@]} active login shell(s): \e[31m"$psu"\e[0m"
   done
