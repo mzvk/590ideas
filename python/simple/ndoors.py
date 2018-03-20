63d607ef22f746531fc6250e2abab32cf0693f6b
mzvk <m.zukey@gmail.com>
2018-03-20 20:18:09 +0100

    s

---
+++ python/simple/ndoors.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+# Solution to the 'extended' 100 doors problem
+# MZvk 2018
+
+import sys
+
+DEFAULT_DOORS = 100
+
+try:
+  doors = int(sys.argv[1]) if len(sys.argv[1:]) == 1 else DEFAULT_DOORS
+except ValueError:
+  doors = DEFAULT_DOORS
+
+print "Doors that remained open:"
+print ', '.join([str(door) for door in xrange(1, doors + 1) if not (door ** 0.5) % 1])
