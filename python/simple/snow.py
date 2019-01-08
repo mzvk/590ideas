b6a7220d0c7e1b060d6765cc6f321ee1d76388d3
mzvk <m.zukey@gmail.com>
2019-01-08 21:42:54 +0100

    snow

--- python/simple/snow.py
+++ python/simple/snow.py
@@ -2,14 +2,14 @@
 
 # Snow in the CLI!
 # Issues: if screen has big height, when flakes are colored
-# screen starts to jitter
+# screen starts to jitter.
 # MZvk 2019
 
 import sys, time, os, random
 
 rows, cols = [int(x) for x in os.popen('stty size', 'r').read().split()]
 linebuffer = [' ' * cols] * rows
-flaketype = '*#.,` -~\'oO0'
+flaketype = '*#.,` -~oO0'
 flakeclr  = ['97', '37', '90', '30', '96', '94']
 
 def getflake():
@@ -17,11 +17,12 @@ def getflake():
   return '{}{}{}'.format(' ' * lshft, flaketype[random.randint(0, len(flaketype)) % len(flaketype)], ' ' * (3 - lshft))
 
 def trimline(line):
+# comment below line, to enable color
   return line[:cols]
-#  nline = ''
-#  for chrs in line[:cols]:
-#    nline = '{}\033[{}m{}\033[0m'.format(nline, flakeclr[random.randint(0, len(flakeclr)) % len(flakeclr)], chrs) if chrs != ' ' else '{}{}'.format(nline, chrs)
-#  return nline
+  nline = ''
+  for chrs in line[:cols]:
+    nline = '{}\033[{}m{}\033[0m'.format(nline, flakeclr[random.randint(0, len(flakeclr)) % len(flakeclr)], chrs) if chrs != ' ' else '{}{}'.format(nline, chrs)
+  return nline
 
 def genline():
   return ''.join([getflake() for flake in xrange((cols + 1)/4)])
@@ -31,6 +32,6 @@ try:
     linebuffer.pop(0)
     linebuffer.append(trimline(genline()))
     print '\n'.join(reversed(linebuffer))
-    time.sleep(1)
+    time.sleep(2)
 except KeyboardInterrupt:
   sys.exit()
