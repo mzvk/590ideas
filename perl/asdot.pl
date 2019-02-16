3718e2e72dc21e99322e4c0dd60437f62a391ca0
mzvk <m.zukey@gmail.com>
2019-02-16 21:45:28 +0100

    max as-plain fix

--- perl/asdot.pl
+++ perl/asdot.pl
@@ -10,6 +10,6 @@ die "No input.\n"if !$ARGV[0];
 
 if($ARGV[0] =~ m/^([0-9]{0,5})\.([0-9]{0,5})$/ && $1 & $2){ 
   print(($1 << 16) + $2 . "\n"); 
-} elsif($ARGV[0] =~ m/^[1-9][0-9]{0,9}$/ && $ARGV[0] < 4294967295){ 
+} elsif($ARGV[0] =~ m/^[1-9][0-9]{0,9}$/ && $ARGV[0] <= 4294967295){ 
   print(($ARGV[0] >> 16) . "." . ($ARGV[0] & 0xFFFF) . "\n");
 } else { die "ASN Is neither AS-PLAIN nor AS-DOT or value equals 0!\n"; }
