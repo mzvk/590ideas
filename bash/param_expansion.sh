765586640fdd6707910e589a1833b7d9ec5e3374
mzvk <m.zukey@gmail.com>
2019-03-06 15:22:24 +0100

    parameter expansion

---
+++ bash/param_expansion.sh
@@ -0,0 +1,15 @@
+#!/bin/bash --
+#
+# Quick tail&test to show parameter expansion
+# @Zvk : 2019
+#
+
+
+read -p "Wanna go nuts? (y|n): " choice
+[[ $choice =~ ^[YNyn]([Ee][Ss]|[Oo])?$ ]] && choice=${choice:0:1} || unset choice
+choice=${choice:-n}
+
+case $choice in
+   y) echo -e "Yes, I wanna go \033[31mberserk!!\033[0m";;
+   n) echo -e "No I'm cool.";;
+esac
