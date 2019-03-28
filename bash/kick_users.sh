e6d4c4fc71e5f300c0548ba902112e8d1ccba511
mzvk <m.zukey@gmail.com>
2019-03-28 14:08:29 +0100

    small fixes

--- bash/kick_users.sh
+++ bash/kick_users.sh
@@ -30,26 +30,27 @@ if searchflag "${uarg[@]}"; then
     pids+=(${x[1]})
     if [[ ${pidmap[${x[0]}]+_} ]]; then pidmap[${x[0]}]+=" ${x[1]}"; else pidmap[${x[0]}]+=${x[1]}; fi
   done
-  echo "Found ${#pids[@]} active login shell(s) for ${#pidmap[@]} user(s)"
+  echo " Found ${#pids[@]} active login shell(s) for ${#pidmap[@]} user(s)"
   for usr in ${!pidmap[@]}; do
-    printf " %-10s: \e[31m%s\e[0m\n" "$usr" "${pidmap[$usr]}"
+    printf "  %-10s: \e[36m%s\e[0m\n" "$usr" "${pidmap[$usr]}"
   done && echo
 else
   for arg in "${uarg[@]}"; do
-    if [[ -z $(getent passwd | grep -e "^$arg:") ]]; then echo -e "\e[33mUser $arg does not exist on this system.\e[0m" && continue; fi
+    if [[ -z $(getent passwd | grep -e "^$arg:") ]]; then echo -e "\e[33m User $arg does not exist on this system.\e[0m" && continue; fi
     psu=`ps -fu $arg | grep "\-bash$" | awk '{print $2}'`
-    if [[ -z $psu ]]; then echo -e "\e[33mUser $arg does not have active login shells.\e[0m" && continue; fi
+    if [[ -z $psu ]]; then echo -e "\e[33m User $arg does not have active login shells.\e[0m" && continue; fi
     pids+=($psu)
-    echo -e "$arg - $((${#pdu[@]} + 1)) active login shell(s): \e[31m"$psu"\e[0m"
+    echo -e "$arg - $((${#pids[@]})) active login shell(s): \e[31m"$psu"\e[0m"
   done
 fi
+[[ -z $pids ]] && { echo -e "\e[33m PID list empty, terminating. \e[0m"; exit; }
 echo " ** Closing all sessions **"
 for pid in "${pids[@]}"; do
   if [[ $pid == $cpid ]]; then
     echo -e "\e[33mIgnoring current user login shell [$cpid]\e[0m"
   else
-    $sudo echo -n "closing session $pid..." ## sudo'd just so prompt for sudo will not mess print-out :V
+    $sudo echo -n "  - closing session $pid..." ## sudo'd just so prompt for sudo will not mess print-out :V
     error=`$sudo kill -9 $pid 2>&1 | sed 's/.*kill: //'`
-    if [[ -z $error ]]; then echo -e "\e[35mdone\e[0m"; else echo -e "\e[31m$error\e[0m"; fi
+    if [[ -z $error ]]; then echo -e "\e[36mdone\e[0m"; else echo -e "\e[31m$error\e[0m"; fi
   fi
 done
