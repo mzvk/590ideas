6ca89cc597ff1a1df9a897687eeff698c0b09f68
mzvk <m.zukey@gmail.com>
2019-06-05 10:13:47 +0200

    rename

--- perl/simple/custom_padding.pl
+++ perl/simple/custom_padding.pl
@@ -1,14 +1,14 @@
 #!/usr/bin/env perl
 
-## Default padding function
+## Custom padding function
 ## Mzvk 2017
 
 use strict refs;
 use warnings;
 
-sub defpadd {
+sub custpadd {
   my ($tx, $pc, $pl) = @_;
   return $tx . $pc x ( $pl - length($tx) > 0 ? $pl - length($tx) : 0);
 }
 
-print defpadd($ARGV[0], $ARGV[1], $ARGV[2]);
+print custpadd($ARGV[0], $ARGV[1], $ARGV[2]);
