4e416492d8727e76c616b8850bb139e9044c782c
mzvk <m.zukey@gmail.com>
2019-06-05 14:18:53 +0200

    fixing null char handling

--- perl/crypto/b64_enc.pl
+++ perl/crypto/b64_enc.pl
@@ -12,8 +12,8 @@ sub Encode {
   my @b = shift // ('A'..'Z','a'..'z',0..9,'\+');
 
   my $pad = (3 - length($d) % 3) % 3;
-  $d =~ s/(.)(.)?(.)?/$b[unpack('C', $1) >> 2].$b[((unpack('C', $1) & 0x03) << 4) + ((unpack('C', $2?$2:0) & 0xF0) >> 4)].
-                      $b[((unpack('C', $2?$2:0) & 0x0F) << 2) + ((unpack('C', $3?$3:0) & 0xC0) >> 6)].$b[unpack('C', $3?$3:0) & 0x3F]/ge;
+  $d =~ s/(.)(.)?(.)?/$b[unpack('C', $1) >> 2].$b[((unpack('C', $1) & 0x03) << 4) + ($2 ? ((unpack('C', $2) & 0xF0) >> 4) : 0)].
+                      $b[($2 ? ((unpack('C', $2) & 0x0F) << 2) : 0) + ($3 ? ((unpack('C', $3) & 0xC0) >> 6) : 0)].$b[($3 ? unpack('C', $3) & 0x3F : 0)]/ge;
   $d =~ s/.{$pad}$/$p x $pad/e if $pad;
   return $d;
 }
