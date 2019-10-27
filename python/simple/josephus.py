77be6eb2f788cfa09ba625cfbbbc393ed7ed1d01
mzvk <m.zukey@gmail.com>
2019-10-27 12:48:28 +0100

    josephus

---
+++ python/simple/josephus.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+# Solution to the Josephus permutation (problem)
+# Kind of hacky way, not following "math" equation
+# It just wraps first bit to the end of number
+# MZvk 2019
+
+
+# solution to Josephus permutation
+
+import sys, re
+
+if not len(sys.argv[1:]) == 1: sys.exit()
+if not re.match(r'^[0-9]+$', sys.argv[1]): sys.exit()
+
+c = int(sys.argv[1])
+print ((c & ~(2**(len(bin(c))-3))) << 1) + 1
