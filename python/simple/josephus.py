a6156a69805410eb80c60ccc34f3f5f65d234824
mzvk <m.zukey@gmail.com>
2019-10-28 08:32:36 +0100

    better display

--- python/simple/josephus.py
+++ python/simple/josephus.py
@@ -14,4 +14,4 @@ if not len(sys.argv[1:]) == 1: sys.exit()
 if not re.match(r'^[0-9]+$', sys.argv[1]): sys.exit()
 
 c = int(sys.argv[1])
-print ((c & ~(2**(len(bin(c))-3))) << 1) + 1
+print "Position avoiding execution: {}".format(((c & ~(2**(len(bin(c))-3))) << 1) + 1)
