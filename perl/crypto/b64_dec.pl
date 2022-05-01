dec2985174a71e44de9dfdd21f1f721a26c0e852
Maciej Z <m.zukey@gmail.com>
2022-05-01 13:39:57 +0200

    Update b64_dec.pl
    fixed typos in character set and decoder function

--- perl/crypto/b64_dec.pl
+++ perl/crypto/b64_dec.pl
@@ -6,16 +6,16 @@
 use strict;
 use warnings;
 
-my @b64 = ('A'..'Z','a'..'z',0..9,'\+');
+my @b64 = ('A'..'Z','a'..'z',0..9,'+','/');
 my %crypt_b64 = map { $b64[$_] => $_ } 0..$#b64;
 
 sub Decode {
   my $e = shift;
 
-  die "Wrong Base64 string\n" unless (length($e) % 4 == 0 && $e =~ m/^[A-Za-z0-9=+\\]+$/);
+  die "Wrong Base64 string\n" unless (length($e) % 4 == 0 && $e =~ m/^[A-Za-z0-9+\/]+=*$/);
   $e =~ s/=/A/g;
   $e =~ s/(.)(.)(.)(.)/pack('C', ($crypt_b64{$1}  << 2) + ($crypt_b64{$2} >> 4)).
-                       pack('C', (($crypt_b64{$2} & 0x07) << 4) + ($crypt_b64{$3} >> 2)).
+                       pack('C', (($crypt_b64{$2} & 0x0F) << 4) + ($crypt_b64{$3} >> 2)).
                        pack('C', (($crypt_b64{$3} & 0x03) << 6) +  $crypt_b64{$4})/ge;
   return $e;
 }
