0131d46d4e78615d3faf40f6595479644e63c627
Maciej Z <m.zukey@gmail.com>
2022-05-01 19:36:23 +0200

    fixing handling of padding for raw bytes

--- perl/crypto/b64_dec.pl
+++ perl/crypto/b64_dec.pl
@@ -13,11 +13,11 @@ sub Decode {
   my $e = shift;
 
   die "Wrong Base64 string\n" unless (length($e) % 4 == 0 && $e =~ m/^[A-Za-z0-9+\/]+=*$/);
-  $e =~ s/=/A/g;
+  my $p = ($e =~ s/=/A/g);
   $e =~ s/(.)(.)(.)(.)/pack('C', ($crypt_b64{$1}  << 2) + ($crypt_b64{$2} >> 4)).
                        pack('C', (($crypt_b64{$2} & 0x0F) << 4) + ($crypt_b64{$3} >> 2)).
                        pack('C', (($crypt_b64{$3} & 0x03) << 6) +  $crypt_b64{$4})/ge;
-  return $e;
+  return substr($e, 0, -1 * $p);
 }
 
 if($ARGV[0]){
