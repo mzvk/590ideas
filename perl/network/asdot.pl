7078ca4d56bfdc4e6e8ca1cd1d05c5e2c53e9251
taazumab <maciej.zurawski1@swisscom.com>
2023-06-05 12:24:04 +0200

    multiple arguments for asdot

--- perl/network/asdot.pl
+++ perl/network/asdot.pl
@@ -8,8 +8,11 @@ use warnings;
 
 die "No input.\n"if $#ARGV < 0;
 
-if($ARGV[0] =~ m/^([0-9]{0,5})\.([0-9]{0,5})$/ && ($1 | $2) && $1 < 65536 && $2 < 65536){
-  print(($1 << 16) + $2 . "\n"); 
-} elsif($ARGV[0] =~ m/^[1-9][0-9]{0,9}$/ && $ARGV[0] <= 4294967295){
-  print(($ARGV[0] >> 16) . "." . ($ARGV[0] & 0xFFFF) . "\n");
-} else { die "ASN Is neither AS-PLAIN [1-4294967295] nor AS-DOT [(0-65535).(0-65535)] or equals 0!\n"; }
+for(@ARGV){
+  my $org = $_;
+  if(m/^([0-9]{0,5})\.([0-9]{0,5})$/ && ($1 | $2) && $1 < 65536 && $2 < 65536){
+    printf "%s: %s\n", $org, ($1 << 16) + $2;
+  } elsif(m/^[1-9][0-9]{0,9}$/ && $_ <= 4294967295){
+    printf "%s: %s\n", $org, sprintf "%d.%d", ($_ >> 16), ($_ & 0xFFFF);
+  } else { die "ASN Is neither AS-PLAIN [1-4294967295] nor AS-DOT [(0-65535).(0-65535)] or equals 0!\n"; }
+}
